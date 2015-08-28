d63f33f2dbae5eb985f66a8ec489f26df9e31fd2
G-h-o-s-t <ghost@shutdown.ru>
2015-08-28 16:24:14 +0300

    qrQode popUp

--- js/wallets.js
+++ js/wallets.js
@@ -5,7 +5,28 @@ var wallets = angular.module('wallets', []);     //"checklist-model" 'ngSanitize
 
 wallets.controller('WalletsCtrl', function ($scope) {
     $scope.tab =  1;
+    $scope.qr ='';
+    $scope.shQr = false;
+
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        width : 280,
+        height : 280
+    });
+
     $scope.switchTab = function(el){ $scope.tab = el; };
+
+    $scope.showQrPopUp = function(qr) {
+        $scope.qr = qr;
+
+        qrcode.clear();
+        qrcode.makeCode(qr);
+        $scope.shQr = true;
+    };
+
+    $scope.closeQrPupUp = function() {
+        $scope.shQr = false;
+        qrcode.clear();
+    }
 
 });
 
